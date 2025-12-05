ad0c42af5d0632480423307b89443dee51aa0f64
Balaji Rajmohan <balajirajmohanbr@gmail.com>
2025-12-05 19:00:07 +0530

    Adding some key changes

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'warn',
+    'no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
+
